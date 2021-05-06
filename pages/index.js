f8edfa6aa6b1730baadd523c78898212abed1e5f
Samit Koyom <samitkoyom@gmail.com>
2021-05-06 18:57:49 +0700

    update home page again

--- pages/index.js
+++ pages/index.js
@@ -17,7 +17,7 @@ export default function Home() {
         </h1>
 
         <p className={styles.description}>
-          เริ่มใช้งานกันที่นี่ {' '}
+          เริ่มใช้งานกันที่นี่นะครับ {' '}
           <code className={styles.code}>pages/index.js</code>
         </p>
 
